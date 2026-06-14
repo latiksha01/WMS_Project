27e596f6f49a605dc5e96bab7d97c14d7414e163
Latiksha Prajapat <latiksha01@gmail.com>
2026-06-14 21:08:10 +0530

    Update API endpoint for RoleService

--- WMS-UI/src/app/core/services/role.service.ts
+++ WMS-UI/src/app/core/services/role.service.ts
@@ -5,7 +5,7 @@ import { Role } from '../models/role';
 
 @Injectable({ providedIn: 'root' })
 export class RoleService {
-  private api = 'https://localhost:7072/api/Role';
+  private api = 'https://wms-api-latiksha-bmhddyhzfydqdtee.southeastasia-01.azurewebsites.net/api/Role';
   constructor(private http: HttpClient) {}
   getAll(): Observable<Role[]> { return this.http.get<Role[]>(this.api); }
   getById(id: number): Observable<Role> { return this.http.get<Role>(`${this.api}/${id}`); }
