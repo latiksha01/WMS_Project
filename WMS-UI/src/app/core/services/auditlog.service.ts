097c65eed36adc9eb2268a8f99eaef5532b617e6
Latiksha Prajapat <latiksha01@gmail.com>
2026-06-14 21:03:26 +0530

    Update API endpoint for AuditLog service

--- WMS-UI/src/app/core/services/auditlog.service.ts
+++ WMS-UI/src/app/core/services/auditlog.service.ts
@@ -5,7 +5,7 @@ import { AuditLog } from '../models/audit-log';
 
 @Injectable({ providedIn: 'root' })
 export class AuditLogService {
-  private api = 'https://localhost:7072/api/AuditLog';
+  private api = 'https://wms-api-latiksha-bmhddyhzfydqdtee.southeastasia-01.azurewebsites.net/api/AuditLog';
   constructor(private http: HttpClient) {}
   getAll(): Observable<AuditLog[]> { return this.http.get<AuditLog[]>(this.api); }
   getById(id: number): Observable<AuditLog> { return this.http.get<AuditLog>(`${this.api}/${id}`); }
